5768025bdbf230bbb10ebc0d33d8c8aff52277dc
suganya153 <suganya.muralikrishnan93@gmail.com>
2025-04-10 15:55:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,24 @@ variable "var4" {}
 
 
 # Use the variables in the local file resource
-resource "local_file" "example" {
-  content  = "Hello, Terraform! Var1: ${var.var1}, Var2: ${var.var2}, Var3: ${var.var3}, Var4: ${var.var4}"
-  filename = "${path.module}/hello-${var.var3}.txt"
+#resource "local_file" "example" {
+#  content  = "Hello, Terraform! Var1: ${var.var1}, Var2: ${var.var2}, Var3: ${var.var3}, Var4: ${var.var4}"
+#  filename = "${path.module}/hello-${var.var3}.txt"
+#}
+
+resource "azurerm_resource_group" "example" {
+  name     = "tfe_np_ws_poc"
+  location = "westus"
+}
+ 
+resource "azurerm_storage_account" "example" {
+  name                     = "tfenpstatefilepoc"
+  resource_group_name = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+ 
+  tags = {
+    environment = "dev"
+  }
 }
