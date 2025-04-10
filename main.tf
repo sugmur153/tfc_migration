99ea7c2ab77d15f0fbe3cda9d14744441edf0ba7
suganya153 <suganya.muralikrishnan93@gmail.com>
2025-04-10 10:42:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "3.8.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "tf_provisioning"
+    storage_account_name = "tfcmigrationpoc"
+    container_name       = "tfc-migration-poc"
+    key                  = "test1.tfstate"
+    access_key           = "4/kct+40HxKweY2VofS7Ha+Dzj6qEdlIBKtpRB9BPn4lmspsQIOpjTGEG5ww04S8LSUZ26OZfvzv+AStAzo1jA=="
+  }
 }
 
 provider "azurerm" {
@@ -20,6 +28,7 @@ variable "var1" {}
 variable "var2" {}
 variable "var3" {}
 variable "var4" {}
+
 
 # Use the variables in the local file resource
 resource "local_file" "example" {
