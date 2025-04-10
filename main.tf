ad71448875c5b3cfc07f6c2b285e63aa4689c279
suganya153 <suganya.muralikrishnan93@gmail.com>
2025-04-10 23:23:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     resource_group_name = "tf_provisioning"
     storage_account_name = "tfcmigrationpoc"
     container_name       = "tfc-migration-poc"
-    key                  = "test1.tfstate"
+    key                  = "tfe_np_ws_poc1.tfstate"
     access_key           = "4/kct+40HxKweY2VofS7Ha+Dzj6qEdlIBKtpRB9BPn4lmspsQIOpjTGEG5ww04S8LSUZ26OZfvzv+AStAzo1jA=="
   }
 }
