ffe22501dff6fa5d5c91d0aa797fdfa137e116ef
suganya153 <suganya.muralikrishnan93@gmail.com>
2025-04-15 19:51:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ terraform {
     }
   }
 
-  #backend "azurerm" {
-  #  resource_group_name = "tf_provisioning"
-  #  storage_account_name = "tfcmigrationpoc"
-  # container_name       = "tfc-migration-poc"
-  # key                  = "tfe_np_ws_poc1.tfstate"
-    #access_key           = "4/kct+40HxKweY2VofS7Ha+Dzj6qEdlIBKtpRB9BPn4lmspsQIOpjTGEG5ww04S8LSUZ26OZfvzv+AStAzo1jA=="
-  #}
+  backend "azurerm" {
+    resource_group_name = "tf_provisioning"
+    storage_account_name = "tfcmigrationpoc"
+    container_name       = "tfc-migration-poc"
+    key                  = "tfe_np_ws_poc1.tfstate"
+    access_key           = "4/kct+40HxKweY2VofS7Ha+Dzj6qEdlIBKtpRB9BPn4lmspsQIOpjTGEG5ww04S8LSUZ26OZfvzv+AStAzo1jA=="
+  }
 }
 
 provider "azurerm" {
